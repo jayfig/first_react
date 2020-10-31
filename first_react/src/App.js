96239ad1db3f0dd3d7b35be02a41950a8e07187d
jayfig <jfiguer5@gmail.com>
2020-10-31 18:23:45 -0400

    First React APP

---
+++ first_react/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb the world. </li>
+        <li>Run faster than Usain Bolt</li>
+        <li>Feed the hungry</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
